51688e3ad5b2e8481d5f21c924f405dd4212a084
DAVID LEE <lileilei999@gmail.com>
2012-12-19 22:51:42 +0700

    Update spidehao123.sh
    Use href pattern instead of http

--- spidehao123.sh
+++ spidehao123.sh
@@ -7,6 +7,5 @@
 # 4.1 cat $(find www.hao123.com/ -type f) | grep -ioP "http://[a-zA-Z0-9\-\_]{1,}\.[a-zA-Z0-9\-\_]{1,}\.(com|net|org|info|cc|edu|gov|int|mil|biz|name|cn|tel)[\/\" ]"|sort -u |sed 's/"//g' >hao123.url
 
 wget -v -nv -nc -c -t3 -np -r -l4  -o hao13.log -e robots=off -U "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; GTB5)" --random-wait -R "png,jpg,jpeg,bmp,gif,js,css,ico,mp3,flv,PNG,JPG,JPEG,BMP,GIF,JS,CSS,ICO,MP3,FLV" http://www.hao123.com
-cat $(find www.hao123.com/ -type f) | grep -ioP "http://[a-zA-Z0-9\-\_]{1,}\.[a-zA-Z0-9\-\_]{1,}\.(com|net|org|info|cc|edu|gov|int|mil|biz|name|cn|tel)\/[\"a-zA-Z0-9\-\_]{1,}"|sort -u |sed 's/"//g' >hao123.url
-
+cat $(find www.hao123.com/ -type f) | grep -io "href=\".*?.*?\""  | grep -v "[#]" | grep "http" |sort -u | grep -io "http.*" |sed 's/"//g' > hao123.url.txt
 
